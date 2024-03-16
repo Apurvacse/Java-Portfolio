101c2375286b458141591e631cffc290dc091fc4
Apurvacse <161112567+Apurvacse@users.noreply.github.com>
2024-03-16 16:51:54 +0530

    Implement SRP principle by using packages to simulate libery book Issuing transaction

---
+++ IssueBook.java
@@ -0,0 +1,17 @@
+package week6;
+import week6.Student;
+import week6.RequestBook;
+
+public class IssueBook {
+    private RequestBook rb;
+
+    public IssueBook(RequestBook rb) {
+        this.rb = rb;
+    }
+
+    public void issubook() {
+        System.out.println("issuing the book");
+        System.out.println("with request id as " + this.rb.getrequestID() + " being issued to " + this.rb.getStudent().getname());
+        System.out.println("book is to be delivered to: " + this.rb.getStudent().getregisterNo());
+    }
+}
---
+++ Student.java
@@ -0,0 +1,21 @@
+package week6;
+public class Student {
+    private String name;
+    private String registerNO;
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getregisterNo() {
+        return registerNO;
+    }
+
+    public void setregisterNO(String rno) {
+        this.registerNO = rno;
+    }
+}
